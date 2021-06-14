03697d4f3806ca6a1ffb67c3cdc92ca6c36d42b0
Alex Smirnou <alex.snirnou94@gmail.com>
2021-06-14 21:51:16 +0300

    Clone Algorithm 1 test

--- src/main/kotlin/subtask3/StringParser.kt
+++
@@ -1,9 +0,0 @@
-package subtask3
-
-class StringParser {
-
-    // TODO: Complete the following function
-    fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
-    }
-}
